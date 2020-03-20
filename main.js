f42d391b8ce48edca328a652d6684584f105e22f
Umberto Del Piano <umberdelpi1@gmail.com>
2020-03-20 18:36:16 +0100

    Esercizio 1 finito

--- main.js
+++ main.js
@@ -8,14 +8,17 @@ alert(" Ciao ti diamo il benvenuto " + nomeUtente)
 var numero1 = parseInt(prompt("Inserisci un numero "));
 var numero2 = parseInt(prompt("Inserisci nuovamente un numero"));
 var numeroMaggiore;
+var numeroUguale;
 
 console.log("Primo numero inserito: " + numero1);
 console.log("Secondo numero inserito: " + numero2);
 
 if (numero1 > numero2) {
-  numero1 = numeroMaggiore + "Il numero maggiore è: "
+  numeroMaggiore = numero1
 } else if (numero2 > numero1) {
-  numero2 = numeroMaggiore +  "Il numero maggiore è: "
+  numeroMaggiore = numero2
+} else {
+  numeroMaggiore = "I numeri sono uguali"
 }
 // Output software
 document.getElementById('js-id').innerHTML="Il numero maggiore è: " + numeroMaggiore;
